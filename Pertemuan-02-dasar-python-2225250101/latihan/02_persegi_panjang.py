8ef606bbe6af5473ed4115af3eecdf50e9bf8fa0
Revaliyanasari <djongsiuaphin@gmail.com>
2026-09-10 08:12:24 +0700

    Delete Pertemuan-02-dasar-python-2225250101 directory

--- Pertemuan-02-dasar-python-2225250101/latihan/02_persegi_panjang.py
+++
@@ -1,8 +0,0 @@
-panjang = float(input("Masukkan panjang: "))
-lebar = float(input("Masukkan lebar: "))
-
-luas = panjang * lebar
-keliling = 2 * (panjang + lebar)
-
-print(f"Luas = {luas:.2f} satuan persegi")
-print(f"Keliling = {keliling:.2f} satuan")
